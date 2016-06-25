ac1fb6d73e1db68722572fbb68a1e37858b47c06
LHB <lhb@LHBdeMacBook-Pro.local>
2016-06-25 14:01:25 +0800

    no message

--- AllToolsAndExtension/AllToolsAndExtension/AppDelegate.m
+++ AllToolsAndExtension/AllToolsAndExtension/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LHBMainViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[LHBMainViewController alloc] init]];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ AllToolsAndExtension/AllToolsAndExtension/LHBMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  LHBMainViewController.h
+//  AllToolsAndExtension
+//
+//  Created by LHB on 16/6/25.
+//  Copyright © 2016年 LHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHBMainViewController : UIViewController
+
+@end
